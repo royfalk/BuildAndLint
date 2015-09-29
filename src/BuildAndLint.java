2a89d0adb8604a229a29ecdd92f2c03ff2cafb6a
royfalk <roy@falk.co.il>
2015-09-29 22:36:49 +0300

    Initial commit after configuring

---
+++ src/BuildAndLint.java
@@ -0,0 +1,19 @@
+import com.intellij.ide.DataManager;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataContext;
+
+/**
+ * Created by user on 9/27/2015.
+ */
+public class BuildAndLint extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        AnAction anAction = ActionManager.getInstance().getAction("CompileProject");
+
+        DataContext context = e.getDataContext(); //DataManager.getInstance().getDataContext();
+        AnActionEvent anActionEvent = new AnActionEvent(null, context, "", anAction.getTemplatePresentation(), ActionManager.getInstance(), 0);
+
+        anAction.actionPerformed(anActionEvent);
+    }
+}
